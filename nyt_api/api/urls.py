9dec812feb9bbe366f3b62334306827efd89d6d9
martin-martin <breuss.martin@gmail.com>
2018-10-16 11:49:15 +0200

    initial commit

--- nyt_api/api/urls.py
+++ nyt_api/api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.frontpage, name='frontpage')
+    path('', views.frontpage, name='frontpage'),
+    path('profile/', views.profile, name='profile'),
+    path('signout/', views.signout, name='signout'),
 ]
--- nyt_api/api/views.py
+++ nyt_api/api/views.py
@@ -1,6 +1,42 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def frontpage(request):
-    return HttpResponse("hei")
+    if request.user.is_authenticated:
+        return redirect('/profile')
+
+    if request.method == 'POST':
+        if 'signupform' in request.POST:
+                signupform = UserCreationForm(data=request.POST)
+                signinform = AuthenticationForm()
+
+                if signupform.is_valid():
+                    username = signupform.cleaned_data['username']
+                    password = signupform.cleaned_data['password1']
+                    signupform.save()
+                    return redirect('/')
+        else:
+            signupform = UserCreationForm()
+            signinform = AuthenticationForm(data=request.POST)
+
+            if signinform.is_valid():
+                login(request, signinform.get_user())
+                return redirect('/')
+
+
+    signupform = UserCreationForm()
+    signinform = AuthenticationForm()
+
+    return render(request, 'frontpage.html', {'signupform': signupform,
+                                              'signinform': signinform})
+
+
+def signout(request):
+    logout(request)
+    return redirect('/')
+
+
+def profile(request):
+    return render(request, 'profile.html')
